05eb19ed9d9cfa868f3f2bb3ea1ffc8893d3e648
Matheus Alves <matheus.alvesrocha@hotmail.com>
2024-05-27 14:21:01 -0300

    Desafio finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+        System.out.print("Informe o número da agência: ");
+        agencia = sc.next();
+
+        System.out.print("Informe o número da conta: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Informe o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.print("Informe o saldo inicial da conta: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
